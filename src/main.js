845f8ef18a1ff83679564772a600327493a9535c
Juana Llanje <juana.lc@hotmail.es>
2022-10-08 02:46:28 -0500

    Adding grid in the design

--- src/main.js
+++ src/main.js
@@ -7,17 +7,17 @@ const container = document.getElementById("container")
 // list.innerHTML = `${list1}`+`${list2}`
 
 const listSuculents = suculents.map((product) => {
-    return (product = `
-    <div class="col">
-    <div class="card h-100">
+    return (product = `<div id="table-products">
+    <div id="card" class="card h-100">
     <img class="card-img-top" src=${product.img}></img>
     <div class="card-body">
     <h5 class="card-title">${product.name}</h5>
     <p class="card-text">${product.description}</p>
     <div class="card-footer">
     <small class="text-muted">Imagen de <a href="www.clara.es">www.clara.es</a></small>
-  </div>
-
+    </div>
+    </div>
+    </div>
     </div>
     </div>`)
 })
