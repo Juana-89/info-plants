a75ecd3a796ddd4966f47ae3aa64924d0a7eeae0
unknown <juana.lc@hotmail.es>
2022-10-08 14:13:02 -0500

    Moving data file to src folder

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import data from '../data.json' assert { type: "json" };
+import data from './data.json' assert { type: "json" };
 const suculents = data.suculents;
 
 const container = document.getElementById("container")
