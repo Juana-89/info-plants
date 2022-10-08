9614f1c22f49f95f0b72b1958ad65a1898387cb8
unknown <juana.lc@hotmail.es>
2022-10-08 13:01:57 -0500

    Finishing design project

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ const listSuculents = suculents.map((product) => {
     return (product = `
     <div class="col">
     <div class="card shadow-sm" id=${product.id}>
-    <img class="card-img-top" height="500em" src=${product.img} alt=${product.name}></img>
+    <img class="card-img-top" height="420em" src=${product.img} alt=${product.name}/>
     <div class="card-body">
     <h5 class="card-title">${product.name}</h5>
     <p class="card-text">${product.description}</p>
     <div class="card-footer">
-    <small class="text-muted">Imagen de <a href="www.clara.es">www.clara.es</a></small>
+    <small class="text-muted">Imagen e info de <a href="www.clara.es">www.clara.es</a></small>
     </div>
     </div>
     </div>
